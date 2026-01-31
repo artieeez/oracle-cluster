71504c43b6a8ad0be8ef5d8e330d1baa257708cc
artur <artur.webber@networkme.io>
2026-01-31 10:59:17 -0300

    fix: add cluster public endpoint to terraform output

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "kubeconfig_command" {
   description = "Command to generate kubeconfig for the cluster."
   value       = "oci ce cluster create-kubeconfig --cluster-id ${oci_containerengine_cluster.oke.id} --file $HOME/.kube/config --region ${var.region} --token-version 2.0.0 --kube-endpoint PUBLIC_ENDPOINT"
 }
+
+output "cluster_public_endpoint" {
+  description = "Public Kubernetes API endpoint."
+  value       = oci_containerengine_cluster.oke.endpoints[0].public_endpoint
+}
